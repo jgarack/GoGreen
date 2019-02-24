f6d4a0ed542dfd81f667e0f8a88c157c5da22984
Alexander <alexandervvugt@gmail.com>
2019-02-24 22:30:20 +0100

    Server can accept registering, authentication works

--- src/main/java/server/GreetingController.java
+++ src/main/java/server/GreetingController.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private List<String> users = new ArrayList<String>();
+    private List<String> passes = new ArrayList<String>();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -19,13 +23,28 @@ public class GreetingController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity postController(
+    public ResponseEntity loginResponse(
             @RequestBody AccountMessage account) {
 
-        if(account.getPassword().equals("Admin")){
+        boolean userExists = users.contains(account.getUsername());
+        boolean passAccepted = passes.get(users.indexOf(account.getUsername())).equals(account.getPassword());
+        if(userExists && passAccepted){
             return new ResponseEntity("Hello" + account.getUsername() + "Authenticated", HttpStatus.OK);
+        }else{
+            return new ResponseEntity("Unknown user-password combination.", HttpStatus.UNAUTHORIZED);
         }
+    }
 
-        return new ResponseEntity("Hello" + account.getUsername() + "Authentication failed", HttpStatus.UNAUTHORIZED );
+    @PostMapping("/register")
+    public ResponseEntity registerResponse(
+        @RequestBody AccountMessage account){
+        if(!users.contains(account.getUsername()) && users.size() == passes.size()){
+            users.add(account.getUsername());
+            passes.add(account.getPassword());
+            return new ResponseEntity("Account created for user: " + account.getUsername(), HttpStatus.OK);
+        }else if(users.contains(account.getUsername())){
+            return new ResponseEntity("Chosen username is already taken.", HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity("Your account could not be created", HttpStatus.BAD_REQUEST);
     }
 }
