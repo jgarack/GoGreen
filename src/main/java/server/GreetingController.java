3e27e76eb5bba5a3f3ca579f3cd8da3d56552ccb
Alexander <alexandervvugt@gmail.com>
2019-02-24 16:03:52 +0100

    //WIP Bugfix status 404 (unresolved)

--- src/main/java/server/GreetingController.java
+++ src/main/java/server/GreetingController.java
@@ -4,17 +4,23 @@ package server;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import utility.AccountMessage;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
 
-//    private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
 
     @PostMapping("/login")
     public ResponseEntity postController(
@@ -24,6 +30,6 @@ public class GreetingController {
             return new ResponseEntity("Hello" + account.getUsername() + "Authenticated", HttpStatus.OK);
         }
 
-        return new ResponseEntity("Hello" + account.getUsername() + "Authenticated", HttpStatus.UNAUTHORIZED );
+        return new ResponseEntity("Hello" + account.getUsername() + "Authentication failed", HttpStatus.UNAUTHORIZED );
     }
 }
