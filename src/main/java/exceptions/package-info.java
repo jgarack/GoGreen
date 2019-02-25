e98a50dfaec66a59ae4f28845b3b2bdd46c5496d
Alexander <alexandervvugt@gmail.com>
2019-02-25 18:09:04 +0100

    See previous commit
    Javadoc changes got excluded somehow.

--- src/main/java/exceptions/package-info.java
+++ src/main/java/exceptions/package-info.java
@@ -1,1 +1,12 @@
+/**
+ * A package containing custom exceptions.
+ */
 package exceptions;
+
+/*
+A list of checked exceptions:
+- ServerStatusException
+
+A list of unchecked exceptions:
+- DataConflictException
+ */
