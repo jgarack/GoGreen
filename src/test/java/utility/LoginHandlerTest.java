eecdfd23651f7f9b6248b5dffc4b112c8fe66e7d
Alexander <alexandervvugt@gmail.com>
2019-03-03 23:41:14 +0100

    better coverage i guess, verification.

---
+++ src/test/java/utility/LoginHandlerTest.java
@@ -0,0 +1,9 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginHandlerTest {
+    private static final String USER = "user";
+    private static final String PASS = "pass";
+    private static final String PASS_TO_MD5 = "1a1dc91c907325c69271ddf0c944bc72";
+}
