fbd65ac74ce3d17caf5b3987b8a76ad576c08874
Vidas <V.Bacevicius@student.tudelft.nl>
2019-03-30 20:39:10 +0100

    UpdateRequestTest equal method tests, solved issue #61

--- src/test/java/utility/UpdateRequestTest.java
+++ src/test/java/utility/UpdateRequestTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UpdateRequestTest {
 
-    UpdateRequest test = new UpdateRequest("user",1,1 );
+    UpdateRequest test = new UpdateRequest("user", 1, 1);
 
     @Test
     void getUsername() {
@@ -21,6 +21,22 @@ class UpdateRequestTest {
     @Test
     void getAmount() {
         assertEquals(test.getAmount(), 1);
+    }
 
+    @Test
+    void equalsSimilarObject() {
+        UpdateRequest test1 = new UpdateRequest("user", 1, 1);
+        assertTrue(test1.equals(test));
+    }
+
+    @Test
+    void equalsSameObject() {
+        assertTrue(test.equals(test));
+    }
+
+    @Test
+    void equalsDifferentObject() {
+        String testString = "test";
+        assertFalse(test.equals(testString));
     }
 }
