62dd0190b624882ef3743a9a379b0d568c710113
Vidas <V.Bacevicius@student.tudelft.nl>
2019-03-31 12:58:45 +0200

    Revert "Pushing FAILING tests for MainHandler. Need help with seeing what's wrong (NumberFormatExceptions, NullPointerExceptions)"
    This reverts commit 8289b7ae743f39bd8787b3f5244c86862685a438.

--- src/test/java/utility/MainHandlerTest.java
+++ src/test/java/utility/MainHandlerTest.java
@@ -10,9 +10,7 @@ import java.io.BufferedReader;
 import java.io.StringReader;
 import java.nio.Buffer;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 public class MainHandlerTest {
@@ -22,14 +20,11 @@ public class MainHandlerTest {
     private static final BufferedReader BR_ONE = new BufferedReader(
             new StringReader("1"));
 
-
     MainHandler testObject;
-
     @BeforeAll
     public static void setUp() throws Exception {
         MainHandler.username = "user";
     }
-
     @BeforeEach
     public void setMocks() {
         HttpRequestHandler mockedHandler = Mockito.mock(HttpRequestHandler.class);
@@ -38,57 +33,6 @@ public class MainHandlerTest {
         testObject.setHttpHandler(mockedHandler);
         testObject.setAlertBuilder(mockedBuilder);
     }
-
-    @Test
-    public void tryParseIntTest() throws Exception {
-        assertTrue(testObject.tryParseInt("500"));
-    }
-
-    @Test
-    public void tryParseIntTestFail() throws Exception {
-        assertFalse(testObject.tryParseInt("hello"));
-    }
-
-    @Test
-    public void updateVeg_succeed() throws Exception {
-        when(testObject.getHttpHandler().reqPost("/points",
-                new UpdateRequest(MainHandler.username, 1, AMOUNT)))
-                .thenReturn(BR_ONE);
-        assertEquals(1, testObject.updateVegMeal(AMOUNT));
-    }
-
-    @Test
-    public void updateBike_succeed() throws Exception {
-        when(testObject.getHttpHandler().reqPost("/points",
-                new UpdateRequest(MainHandler.username, 2, AMOUNT)))
-                .thenReturn(BR_ONE);
-        assertEquals(1, testObject.updateBike(AMOUNT));
-    }
-
-    @Test
-    public void updateLocal_succeed() throws Exception {
-        when(testObject.getHttpHandler().reqPost("/points",
-                new UpdateRequest(MainHandler.username, 3, AMOUNT)))
-                .thenReturn(BR_ONE);
-        assertEquals(1, testObject.updateLocal(AMOUNT));
-    }
-
-    @Test
-    public void updateBus_succeed() throws Exception {
-        when(testObject.getHttpHandler().reqPost("/points",
-                new UpdateRequest(MainHandler.username, 4, AMOUNT)))
-                .thenReturn(BR_ONE);
-        assertEquals(1, testObject.updateBus(AMOUNT));
-    }
-
-    @Test
-    public void updateSolar_failure() throws Exception {
-        when(testObject.getHttpHandler().reqPost("/points",
-                new UpdateRequest(MainHandler.username, 5, AMOUNT)))
-                .thenReturn(BR_FALSE);
-        assertEquals(0, testObject.updateSolar(AMOUNT));
-    }
-
     @Test
     public void updateSolar_succeed() throws Exception {
         when(testObject.getHttpHandler().reqPost("/points",
@@ -96,5 +40,11 @@ public class MainHandlerTest {
                 .thenReturn(BR_ONE);
         assertEquals(1, testObject.updateSolar(AMOUNT));
     }
-
+    @Test
+    public void updateSolar_failure() throws Exception {
+        when(testObject.getHttpHandler().reqPost("/points",
+                new UpdateRequest(MainHandler.username, 5, AMOUNT)))
+                .thenReturn(BR_FALSE);
+        assertEquals(0, testObject.updateSolar(AMOUNT));
+    }
 }
